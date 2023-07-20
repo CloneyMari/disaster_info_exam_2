b904606aaa71039c9a134f832d9d5de5ba334001
CloneyMari <cloneymaricarandang@gmail.com>
2023-07-21 02:30:40 +0800

    create city model, build service and add fetch cities method

--- app/models/address/province.rb
+++ app/models/address/province.rb
@@ -2,5 +2,6 @@ class Address::Province < ApplicationRecord
   validates :name, presence: true
   validates :code, uniqueness: true
 
-  belongs_to :region 
+  belongs_to :region
+  has_many :cities
 end
