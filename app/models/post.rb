00084a507759ad13b2337b0b350e37efdae98239
CloneyMari <cloneymaricarandang@gmail.com>
2023-07-20 15:30:23 +0800

    add associations and category name validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  has_many :post_category_ships
+  has_many :categories, through: :post_category_ships
 end
--- app/models/post_category_ship.rb
+++ app/models/post_category_ship.rb
@@ -1,2 +1,4 @@
 class PostCategoryShip < ApplicationRecord
+ has_many :post_category_ships
+ has_many :categories, through: :post_category_ships
 end
