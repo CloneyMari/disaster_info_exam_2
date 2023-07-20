457fa7bf4fef37a842e9e0419faf2e572a782c10
CloneyMari <cloneymaricarandang@gmail.com>
2023-07-20 14:40:05 +0800

    done CRUD for post and add validations

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-root 'welcome#index'
-resources :posts
+  root 'welcome#index'
+  resources :posts
 end
